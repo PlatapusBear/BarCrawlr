0d125a029d1d650531ef542e4a44ceb30f08f09b
KRWeidner <ktabe13@gmail.com>
2018-10-17 18:06:34 -0500

    added swipe cards

---
+++ app/src/main/java/com/barcrawlr/barcrawlr/CardAdapter.java
@@ -0,0 +1,86 @@
+package com.barcrawlr.barcrawlr;
+
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private List<CardInfo> cData;
+
+    public CardAdapter(Activity activity, ArrayList<CardInfo> data) {
+        this.cData = data;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        return cData.size();
+    }
+
+    @Override
+    public CardInfo getItem(int position) {
+        return cData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            // inflate UI from XML file
+            convertView = inflater.inflate(R.layout.info_card, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.name.setText(getItem(position).getName());
+        holder.location.setText(getItem(position).getLocation());
+        holder.barProfile.setImageBitmap(decodeBitmapResource(activity.getResources(),
+                getItem(position).getDrawableId(),150,300));
+
+        return convertView;
+    }
+
+    private Bitmap decodeBitmapResource(Resources resources, int drawableId, int i, int i1) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(resources,drawableId,options);
+        //
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(resources, drawableId, options);
+    }
+
+
+    private class ViewHolder{
+        private ImageView barProfile;
+        private TextView name;
+        private TextView location;
+
+        public ViewHolder(View view) {
+            barProfile = (ImageView)view.findViewById(R.id.barprofile);
+            name = (TextView)view.findViewById(R.id.name);
+            location = (TextView)view.findViewById(R.id.location);
+        }
+    }
+
+}
--- app/src/main/java/com/barcrawlr/barcrawlr/MainActivity.java
+++ app/src/main/java/com/barcrawlr/barcrawlr/MainActivity.java
@@ -2,12 +2,54 @@ package com.barcrawlr.barcrawlr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import link.fls.swipestack.SwipeStack;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeStack cardStack;
+    private CardAdapter cardAdapter;
+    private ArrayList<CardInfo> cdInfo;
+    private int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardStack = (SwipeStack) findViewById(R.id.container);
+
+        setCardStackAdapter();
+        currentPosition = 0;
+
+        cardStack.setListener(new SwipeStack.SwipeStackListener() {
+            @Override
+            public void onViewSwipedToLeft(int position) {
+                currentPosition = position + 1;
+            }
+
+            @Override
+            public void onViewSwipedToRight(int position) {
+                currentPosition = position + 1;
+            }
+
+            @Override
+            public void onStackEmpty() {
+
+            }
+        });
+
+    }
+
+    private void setCardStackAdapter() {
+        cdInfo = new ArrayList<>();
+
+        cdInfo.add(new CardInfo(R.drawable.ic_launcher_background,"Bar 1","Distance Away"));
+        cdInfo.add(new CardInfo(R.drawable.ic_launcher_background,"Bar 2","Distance Away"));
+        cdInfo.add(new CardInfo(R.drawable.ic_launcher_background,"Bar 3","Distance Away"));
+
+        cardAdapter = new CardAdapter(this,cdInfo);
+        cardStack.setAdapter(cardAdapter);
     }
 }
