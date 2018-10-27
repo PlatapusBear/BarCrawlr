cdda0f06e55fc5ac70210fe66893918ef6223c36
Nick_Ship <nickshipley5@gmail.com>
2018-10-27 14:29:59 -0500

    Maps added to button
    All of the maps work I have done up to this point, the 4 classes, are now depreciated in regards to our group code.
    
    Disregard the 4 classes pertaining to the maps/

--- app/src/main/java/com/barcrawlr/barcrawlr/FragmentMaps.java
+++ app/src/main/java/com/barcrawlr/barcrawlr/FragmentMaps.java
@@ -1,6 +1,7 @@
 package com.barcrawlr.barcrawlr;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,17 +10,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.Serializable;
 import java.util.Map;
 public class FragmentMaps extends Fragment {
     View view;
     private Button MapButton;
+
     public FragmentMaps() {
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.maps_fragment, container, false);
+        MapButton = (Button) view.findViewById(R.id.MapButton);
+
+        MapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String bar= getActivity().getIntent().getExtras().getString("BarName");
+
+
+                String[] parts = bar.split(" ");
+
+
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("google.navigation:q=" + parts[0] + "+" + parts[1] + "+Des+Moines+Iowa"));
+                startActivity(intent);
+                /** Intent intent = new Intent(getActivity(), MapsActivity.class);
+
+                 intent.putExtra("Maps",(Serializable)"Ingersol+Tap");
+                 startActivity(intent);**/
+            }
+        });
+        /** Bar bar = new Bar();
+
+         Intent intent = new Intent(getContext(), MapsActivity.class);
+
+         intent.putExtra("Maps",(Serializable)bar.getName());
+         startActivity(intent);**/
         return view;
+
+
         //MapButton.setOnClickListener(new View.OnClickListener()) {
         //@Override
         //public void onClick(View view) {
@@ -27,5 +60,22 @@ public class FragmentMaps extends Fragment {
         //intent.putExtra("Bar", "location");
         //startActivity(intent);
         //  };
+    }
+
+    public void onClickListener()
+
+    {
+        MapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bar bar = new Bar();
+
+                Intent intent = new Intent(getContext(), MapsActivity.class);
+
+                intent.putExtra("Maps",(Serializable)bar.getName());
+                startActivity(intent);
+            }
+        });
+
     }
 }
